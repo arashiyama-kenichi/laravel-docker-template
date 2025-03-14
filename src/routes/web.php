883368d1812ea20df1a36af4a6d546b53c60f289
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 <arashiyamasoccer@gmail.com>
2025-03-14 16:25:29 +0900

    remove: .DS_Storeの削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,3 @@ Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::post('/todo', 'TodoController@store')->name('todo.store');
 Route::get('/todo', 'TodoController@index')->name('todo.index');
 
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
