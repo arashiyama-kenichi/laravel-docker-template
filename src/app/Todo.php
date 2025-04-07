a478a4199409a618b43fcf3c3ef241bcc6ee9aca
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 <arashiyamasoccer@gmail.com>
2025-04-08 07:39:26 +0900

    メモ更新

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    protected $table = 'todos';
+    protected $table = 'todos'; # Mapping(関連付け)
 
     protected $fillable = [
         'content',
