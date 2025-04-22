8b050eec5d27a5b98c8caa7376724753ce94f8b5
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 <arashiyamasoccer@gmail.com>
2025-04-22 17:50:37 +0900

    feat:論理削除機能の実装

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+    use SoftDeletes;
+    
     protected $table = 'todos'; # todosテーブルとMapping(DBとの関連付け)
 
     protected $fillable = [
