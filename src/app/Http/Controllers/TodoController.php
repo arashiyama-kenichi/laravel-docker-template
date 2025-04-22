b5b361ea4d08e2922c487d54bd95071b7ec058c6
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 <arashiyamasoccer@gmail.com>
2025-04-22 17:43:50 +0900

    feat:削除機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -63,4 +63,13 @@ class TodoController extends Controller
         return redirect()->route('todo.show', $todo->id);
     }
 
+    public function delete($id)
+    {
+        // dd('削除のルート実行！' . $id);
+        $todo = $this->todo->find($id);
+        $todo->delete();
+
+        return redirect()->route('todo.index');
+    }
+
 }
